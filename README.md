a62304f9b0bb7a80618db49440c7c353da4d2065
seattlenomnom <mark.crapser@gmail.com>
2025-03-18 01:21:47 -0700

    Last commit for sd16, move on

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ The chapter title is "Statements".
 Wed 12 Mar 2025 09:17:33 PM PDT
 16.1 selection statements
 16.1.1 if statements.
+
+Tue 18 Mar 2025 01:15:01 AM PDT
+A lot of dumb sections in 16. I did enjoy using the trigraph ?:
+x = conditon ? expression1 : expression2. An example: 
+x = condition ? 1 : 0;
+
+16.2 Iteration statements I remember, I remember they are more capable than
+this text is letting on. Try to remember to experiment with For, While, and 
+Do While constructions.
--- sd16.cpp
+++ sd16.cpp
@@ -25,16 +25,61 @@
 int main() {
 
     bool b = true;
+    bool finished = false;
+    bool myCondition = false;
+    int x = 0;
+    bool a = true;
+    bool b2 = true;
 
     std::cout << "language version: " << __cplusplus << std::endl;
-/*
+
     if(b)
-        std::cout << "The condition is true\n";
-*/
-    if(!b)
-        std::cout << "The condition is true.\n";
+        std::cout << "The condition is true version 1\n";
+
+
+    // the following logic is confusing. Got it!
+    if(!finished)
+        std::cout << "Not finished condition.\n";
     else
-        std::cout << "The condition is false.\n";
+        std::cout << "Finised conition.\n";
+
+    // trigraph section
+    std::cout << "trigraph section: \n";
+
+    x = (myCondition) ? 1 : 0;
+    std::cout << "The value of x is: " << x << '\n';
+
+
+
+/* 16.1.3 The Logical Operators */
+
+    std::cout << "Logical Operators\n";
+
+    if(a && b2)
+    {
+        std::cout << " a AND b is true\n";
+    }else{
+        std::cout << " a AND b is false\n";
+    }
+
+// switch statement
+    std::cout << "switch statement\n";
+    int w = 3;
+    switch (w)
+    {
+        case 1:
+            std::cout << "The value of w is 1\n";
+           break;
+        case 2:
+           std::cout << "The value of w is 2\n";
+          break;
+        case 3:
+          std::cout << "The value of w is 3\n";
+         break;
+        default:
+         std::cout << "The value is none of the above\n";
+         break;
+    }
 
 
 
